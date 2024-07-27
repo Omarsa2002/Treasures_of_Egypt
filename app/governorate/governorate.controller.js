5e3d072710de929063e2e7d8fd587a614f3d5869
Omar Mabrouk <omersa2002@gmail.com>
2024-07-28 01:38:21 +0300

    auth

--- app/governorate/governorate.controller.js
+++ app/governorate/governorate.controller.js
@@ -11,14 +11,30 @@ const { array } = require("joi");
 
 const allGovernorate = async(req,res,next)=>{
     try{
+        const {lang} = req.query
         const governorates = await governorateModele.find().select("governorateName governorateId governorateArName governorateLogo -_id").sort({"governorateName": 1});
-        sendResponse(res,constans.RESPONSE_SUCCESS,"done",governorates,[]);
+        const data = [];
+        governorates.forEach(governorate=>{
+            const dataup = (lang === "en") ? {
+                governorateId: governorate.governorateId,
+                governorateName: governorate.governorateName,
+                governorateLogo: governorate.governorateLogo
+            }:{
+                governorateId: governorate.governorateId,
+                governorateName: governorate.governorateArName,
+                governorateLogo: governorate.governorateLogo
+            }
+            data.push(dataup);
+        })
+        console.log(data);
+        sendResponse(res,constans.RESPONSE_SUCCESS,"done",data,[]);
     }catch(error){
         sendResponse( res,constans.RESPONSE_INT_SERVER_ERROR,error.message,{},constans.UNHANDLED_ERROR);
     }
 }
 const allHistoricalSites = async(req,res,next)=>{
     try{
+        const {lang} = req.query
         const {governorateId} = req.params
         const HistoricalSites = await governorateModele.findOne({governorateId})
         const data = HistoricalSites.HistoricalSites
@@ -26,14 +42,17 @@ const allHistoricalSites = async(req,res,next)=>{
         //console.log(data);
         let updateData = data.map(site =>{
             const { _id, ...rest } = site.toObject({getters: true});
-            return {
+            return (lang === 'en')? {
                 siteId: rest.siteId,
-                en_Site_Name: rest.en_Site_Name,
-                en_Opening_Hours: rest.en_Opening_Hours,
-                ar_Site_Name: rest.ar_Site_Name,  
-                ar_Opening_Hours: rest.ar_Opening_Hours,  
-                Photo_URL: rest.Photo_URL, 
-            };
+                Site_Name: rest.en_Site_Name,
+                Opening_Hours: rest.en_Opening_Hours,
+                Photo_URL: rest.Photo_URL
+            }:{
+                siteId: rest.siteId,
+                Site_Name: rest.ar_Site_Name,
+                Opening_Hours: rest.ar_Opening_Hours,
+                Photo_URL: rest.Photo_URL
+            }
         })
         sendResponse(res,constans.RESPONSE_SUCCESS,"done",updateData,[]);
     }catch(error){
@@ -42,20 +61,24 @@ const allHistoricalSites = async(req,res,next)=>{
 }
 const allRecreationalSites = async(req,res,next)=>{
     try{
+        const {lang} = req.query
         const {governorateId} = req.params
         const RecreationalSites = await governorateModele.findOne({governorateId})
         const data = RecreationalSites.RecreationalSites
         data.sort((a,b) => {return (a.en_Site_Name > b.en_en_Site_Name) ? 1 : ((b.en_Site_Name > a.en_Site_Name) ? -1 : 0)})
         let updateData = data.map(site =>{
             const { _id, ...rest } = site.toObject({getters: true});
-            return {
+            return (lang === 'en')? {
                 siteId: rest.siteId,
-                en_Site_Name: rest.en_Site_Name,
-                en_Opening_Hours: rest.en_Opening_Hours,
-                ar_Site_Name: rest.ar_Site_Name,
-                ar_Opening_Hours: rest.ar_Opening_Hours,
+                Site_Name: rest.en_Site_Name,
+                Opening_Hours: rest.en_Opening_Hours,
                 Photo_URL: rest.Photo_URL
-            };
+            }:{
+                siteId: rest.siteId,
+                Site_Name: rest.ar_Site_Name,
+                Opening_Hours: rest.ar_Opening_Hours,
+                Photo_URL: rest.Photo_URL
+            }
         })
         sendResponse(res,constans.RESPONSE_SUCCESS,"done",updateData,[]);
     }catch(error){
@@ -74,35 +97,35 @@ const HistoricalSites = async(req,res,next)=>{
             return (lang === 'en')? {
                 Location: rest.Location,
                 siteId: rest.siteId,
-                en_Site_Name: rest.en_Site_Name,
-                en_adress: rest.en_adress,
-                en_Historical_Period: rest.en_Historical_Period,
-                en_Description: rest.en_Description,  
-                en_Opening_Hours: rest.en_Opening_Hours,
-                en_Visitor_Statistics: rest.en_Visitor_Statistics,
-                en_Nearby_Amenities: rest.en_Nearby_Amenities, 
-                en_City: rest.en_City, 
-                en_Region: rest.en_Region,
-                en_Average_Temperature: rest.en_Average_Temperature, 
-                en_Best_Visiting_Season: rest.en_Best_Visiting_Season,
-                en_Transportation_Options: rest.en_Transportation_Options, 
+                Site_Name: rest.en_Site_Name,
+                adress: rest.en_adress,
+                Historical_Period: rest.en_Historical_Period,
+                Description: rest.en_Description,  
+                Opening_Hours: rest.en_Opening_Hours,
+                Visitor_Statistics: rest.en_Visitor_Statistics,
+                Nearby_Amenities: rest.en_Nearby_Amenities, 
+                City: rest.en_City, 
+                Region: rest.en_Region,
+                Average_Temperature: rest.en_Average_Temperature, 
+                Best_Visiting_Season: rest.en_Best_Visiting_Season,
+                Transportation_Options: rest.en_Transportation_Options, 
                 Entry_Fee: rest.Entry_Fee  ,
                 Photo_URL: rest.Photo_URL, 
             }:{
                 Location: rest.Location,
                 siteId: rest.siteId,
-                ar_Site_Name: rest.ar_Site_Name,  
-                ar_adress: rest.ar_adress,  
-                ar_Historical_Period: rest.ar_Historical_Period,
-                ar_Description: rest.ar_Description,
-                ar_Opening_Hours: rest.ar_Opening_Hours,  
-                ar_Visitor_Statistics: rest.ar_Visitor_Statistics,
-                ar_Nearby_Amenities: rest.ar_Nearby_Amenities,
-                ar_City: rest.ar_City ,
-                ar_Region: rest.ar_Region,
-                ar_Average_Temperature: rest.ar_Average_Temperature,  
-                ar_Best_Visiting_Season: rest.ar_Best_Visiting_Season , 
-                ar_Transportation_Options: rest.ar_Transportation_Options , 
+                Site_Name: rest.ar_Site_Name,  
+                adress: rest.ar_adress,  
+                Historical_Period: rest.ar_Historical_Period,
+                Description: rest.ar_Description,
+                Opening_Hours: rest.ar_Opening_Hours,  
+                Visitor_Statistics: rest.ar_Visitor_Statistics,
+                Nearby_Amenities: rest.ar_Nearby_Amenities,
+                City: rest.ar_City ,
+                Region: rest.ar_Region,
+                Average_Temperature: rest.ar_Average_Temperature,  
+                Best_Visiting_Season: rest.ar_Best_Visiting_Season , 
+                Transportation_Options: rest.ar_Transportation_Options , 
                 Entry_Fee: rest.Entry_Fee  ,
                 Photo_URL: rest.Photo_URL, 
             }
@@ -124,37 +147,37 @@ const RecreationalSites = async(req,res,next)=>{
             return (lang === 'en')? {
                 Location: rest.Location,
                 siteId: rest.siteId,
-                en_Site_Name: rest.en_Site_Name,
-                en_adress: rest.en_adress,
-                en_Type_of_Activity: rest.en_Type_of_Activity,
-                en_Description: rest.en_Description,
-                en_Opening_Hours: rest.en_Opening_Hours,
-                en_Visitor_Statistics: rest.en_Visitor_Statistics,
-                en_Nearby_Amenities: rest.en_Nearby_Amenities,
-                en_General_Information: rest.en_General_Information,
-                en_City: rest.en_City,
-                en_Region: rest.en_Region,
-                en_Average_Temperature: rest.en_Average_Temperature,
-                en_Best_Visiting_Season: rest.en_Best_Visiting_Season,
-                en_Transportation_Options: rest.en_Transportation_Options,
+                Site_Name: rest.en_Site_Name,
+                adress: rest.en_adress,
+                Type_of_Activity: rest.en_Type_of_Activity,
+                Description: rest.en_Description,
+                Opening_Hours: rest.en_Opening_Hours,
+                Visitor_Statistics: rest.en_Visitor_Statistics,
+                Nearby_Amenities: rest.en_Nearby_Amenities,
+                General_Information: rest.en_General_Information,
+                City: rest.en_City,
+                Region: rest.en_Region,
+                Average_Temperature: rest.en_Average_Temperature,
+                Best_Visiting_Season: rest.en_Best_Visiting_Season,
+                Transportation_Options: rest.en_Transportation_Options,
                 Entry_Fee: rest.Entry_Fee,
                 Photo_URL: rest.Photo_URL
             }:{
                 Location: rest.Location,
                 siteId: rest.siteId,
-                ar_Site_Name: rest.ar_Site_Name,
-                ar_adress: rest.ar_adress,
-                ar_Type_of_Activity: rest.ar_Type_of_Activity,
-                ar_Description: rest.ar_Description,
-                ar_Opening_Hours: rest.ar_Opening_Hours,
-                ar_Visitor_Statistics: rest.ar_Visitor_Statistics,
-                ar_Nearby_Amenities: rest.ar_Nearby_Amenities,
-                ar_General_Information: rest.ar_General_Information,
-                ar_City: rest.ar_City,
-                ar_Region: rest.ar_Region,
-                ar_Average_Temperature: rest.ar_Average_Temperature,
-                ar_Best_Visiting_Season: rest.ar_Best_Visiting_Season,
-                ar_Transportation_Options: rest.ar_Transportation_Options,
+                Site_Name: rest.ar_Site_Name,
+                adress: rest.ar_adress,
+                Type_of_Activity: rest.ar_Type_of_Activity,
+                Description: rest.ar_Description,
+                Opening_Hours: rest.ar_Opening_Hours,
+                Visitor_Statistics: rest.ar_Visitor_Statistics,
+                Nearby_Amenities: rest.ar_Nearby_Amenities,
+                General_Information: rest.ar_General_Information,
+                City: rest.ar_City,
+                Region: rest.ar_Region,
+                Average_Temperature: rest.ar_Average_Temperature,
+                Best_Visiting_Season: rest.ar_Best_Visiting_Season,
+                Transportation_Options: rest.ar_Transportation_Options,
                 Entry_Fee: rest.Entry_Fee,
                 Photo_URL: rest.Photo_URL
             }
